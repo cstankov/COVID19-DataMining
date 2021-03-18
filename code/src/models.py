dfbd700707f2d20934f114238c342fdc23c1477e
Saqib Dhuka <saqibdhuka@gmail.com>
2021-03-17 21:48:43 -0700

    decided on adaboost for boosting classifier

--- code/src/models.py
+++ code/src/models.py
@@ -23,27 +23,45 @@ def adaBoostModel(train_data, val_data):
 
     x_train = x_train.to_numpy()
     y_train = y_train.to_numpy()
-    x_val =x_val.to_numpy()
-    y_val =y_val.to_numpy()
+    x_val = x_val.to_numpy()
+    y_val = y_val.to_numpy()
+   
     # ada = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=3),
     #                            algorithm="SAMME.R",
     #                            n_estimators=50, 
     #                            learning_rate=1, 
-    #                            random_state = 0)
-    # ada = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=3),
-    #                            algorithm="SAMME.R", n_estimators = 200, learning_rate = 1)
+    #                            random_state = 0) # 55 to 59 percent
+   
+    # 57 to 62 percent
+    # ada = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=2),
+    #                            algorithm="SAMME.R", n_estimators = 200, learning_rate = 0.2)
     # model = ada.fit(x_train, y_train)
+    sum_acc = 0
+    for i in range(0, 5): 
+        # 40 to 50 percent
+        # lgbm = LGBMClassifier(n_estimators = 100, 
+        #                     learning_rate = 0.7)
+        # model = lgbm.fit(x_train, y_train) 
 
-    lgbm = LGBMClassifier(n_estimators = 100, learning_rate = 1)
-    model = lgbm.fit(x_train, y_train)
+        # percent
+        ada = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=2),
+                               algorithm="SAMME.R", n_estimators = 200, learning_rate = 0.2)
+        model = ada.fit(x_train, y_train)
 
-    # cbc = CatBoostClassifier(verbose=0, n_estimators=200,)
-    # model = cbc.fit(x_train, y_train)
+        #56 percent
+        # ada = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=2),
+        #                        algorithm="SAMME.R", n_estimators = 50, learning_rate = 1, random_state = 0)
+        # model = ada.fit(x_train, y_train)
+
+        # 45.3 percent
+        # cbc = CatBoostClassifier(depth=10, iterations= 500, l2_leaf_reg= 9, learning_rate= 0.15)
+        # model = cbc.fit(x_train, y_train)
+
+
+        y_pred = model.predict(x_val)
+        sum_acc += metrics.accuracy_score(y_val, y_pred)
     
-    # grad = GradientBoostingRegressor(n_estimators = 100, learning_rate = 1)
-    # grad = GradientBoostingClassifier()
-    # model = grad.fit(x_train, y_train)
+    avg_acc = sum_acc / 5
 
-    y_pred = model.predict(x_val)
-    print("Accuracy of AdaBoost Model: ", metrics.accuracy_score(y_val, y_pred))                    
+    print("Average Accuracy: ", avg_acc)                    
 
